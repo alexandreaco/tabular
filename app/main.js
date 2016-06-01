270997b987a6e688ef6f587a22cc8dcec1b7cbd7
Alex Defreitas <alexandreadef@gmail.com>
2016-06-01 16:30:16 -0400

    init react app with webpack

---
+++ app/main.js
@@ -0,0 +1,6 @@
+import 'babel-polyfill';
+import $ from 'jquery';
+
+import App from './components/App';
+
+var css = require("./scss/main.scss");
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = {
+  entry: {
+    app: ["./app/main.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "app.bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      },
+    ]
+  }
+};
